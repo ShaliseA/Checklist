e605b0a3dd6b084856cf2e662570d5f95e2aea13
Shalise Ayromloo <shaliseayromloo@shalises-macbook-air.local>
2019-03-21 16:19:27 -0500

    This is a checklist app: an app that will allow the user to create a to-do list, where the user may check the tasks off when completed.

---
+++ Checklist/ChecklistItems.swift
@@ -0,0 +1,20 @@
+//
+//  ChecklistItems.swift
+//  Checklist
+//
+//  Created by Shalise Ayromloo on 3/21/19.
+//  Copyright © 2019 Shalise Ayromloo. All rights reserved.
+//
+
+import Foundation
+
+class ChecklistItem {
+    
+   var text = ""
+   var checked = false
+    
+    func toggleChecked() {
+        checked = !checked 
+    }
+    
+} //closes class
